1aa0aa7f6ba04e906a6a85836e207e2acddae754
jacobAGTyler <jacobagtyler@users.noreply.github.com>
2021-02-06 12:26:52 +0000

    Token Validation Listener Fix
    Signed-off-by: jacobAGTyler <jacobagtyler@users.noreply.github.com>

--- tests/TestCase/Listener/TokenListenerTest.php
+++ tests/TestCase/Listener/TokenListenerTest.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 namespace App\Test\TestCase\Listener;
 
+use App\Listener\TokenListener;
 use App\Model\Entity\Token;
 use App\Test\TestCase\ControllerTestCase as TestCase;
+use Cake\Event\Event;
 use Cake\Event\EventList;
 use Cake\Event\EventManager;
 use Cake\I18n\FrozenTime;
@@ -16,6 +18,7 @@ use Cake\I18n\FrozenTime;
  * @property \App\Model\Table\TokensTable $Tokens
  * @property \Queue\Model\Table\QueuedJobsTable $QueuedJobs
  * @property EventManager $EventManager
+ * @property TokenListener $Listener
  */
 class TokenListenerTest extends TestCase
 {
@@ -25,6 +28,8 @@ class TokenListenerTest extends TestCase
         $this->Tokens = $this->getTableLocator()->get('Tokens');
         $this->QueuedJobs = $this->getTableLocator()->get('Queue.QueuedJobs');
 
+        $this->Listener = new TokenListener();
+
         // enable event tracking
         $this->EventManager = EventManager::instance();
         $this->EventManager->setEventList(new EventList());
@@ -33,7 +38,7 @@ class TokenListenerTest extends TestCase
         FrozenTime::setTestNow($now);
     }
 
-    public function testTokenValidated()
+    public function testEventFired()
     {
         $tokenString = $this->Tokens->buildToken(1);
         $token = $this->Tokens->get(1);
@@ -53,5 +58,45 @@ class TokenListenerTest extends TestCase
         TestCase::assertTrue($token->get(Token::FIELD_ACTIVE));
         $cleanState = $this->Tokens->cleanToken($token);
         TestCase::assertEquals(1, $cleanState);
+    }
+
+    public function testListenerFunctionValid()
+    {
+        $beforeToken = $this->Tokens->get(1);
+        TestCase::assertNull($beforeToken->get($beforeToken::FIELD_UTILISED));
+
+        $fakeEvent = $this
+            ->getMockBuilder(Event::class)
+            ->setConstructorArgs([
+                'Model.Tokens.tokenValidated',
+                $this->Tokens,
+                ['tokenId' => $beforeToken->id],
+            ])
+            ->getMock();
+
+        $fakeEvent
+            ->expects(TestCase::any())
+            ->method('getSubject')
+            ->willReturn($this->Tokens);
+
+        $fakeEvent
+            ->expects(TestCase::any())
+            ->method('getData')
+            ->willReturn(1);
+
+        $this->Listener->tokenValidate($fakeEvent);
+
+        $afterToken = $this->Tokens->get(1);
+        TestCase::assertNotSame(
+            $beforeToken->get($beforeToken::FIELD_UTILISED),
+            $afterToken->get($afterToken::FIELD_UTILISED)
+        );
+
+        $jobs = $this->QueuedJobs->find();
+        TestCase::assertSame(1, $jobs->count());
+
+        $job = $jobs->first();
+        TestCase::assertSame('Token', $job->get('job_type'));
+        TestCase::assertNull($job->get('data'));
     }
 }
